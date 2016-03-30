049ed20bb5a30c6963fb6f48b1e26523810e3ac1
Charles <charles@revel.in>
2016-03-30 15:55:34 -0400

    initial commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Charles
+ * Date: 3/30/16
+ * Time: 2:22 PM
+ */
+?>
+
+
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Charles
+ * Date: 3/30/16
+ * Time: 2:21 PM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="style.css" type="text/css">
+<head>
+    <meta charset="UTF-8">
+    <title>League Of Legends</title>
+</head>
+<body>
+<div class="shell">
